9d696e158580c9e72980238abfd22798841e04bf
ijoakin2017 <ijoakin@gmail.com>
2021-08-26 08:31:41 -0300

    New Commit

---
+++ ProductsSolution/DTO/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO
+{
+    public class CategoryDto : BaseDTO
+    {
+        public string description { get; set; }
+
+        public string code { get; set; }
+        public int maxprice { get; set; }
+    }
+}
